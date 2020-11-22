3878c15f50637e790a0ecf2f897a23626d87cecf
Roy Chavarría <roychav99@gmail.com>
2020-11-22 12:01:45 -0600

    Movimiento Jugador (Sin colisión)

---
+++ BomberTEC/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MovementSpeed = 5;
+    private void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX = Input.GetAxis("Horizontal");
+        var moveY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(moveX, moveY, 0) * Time.deltaTime * MovementSpeed;
+    }
+
+    
+}
