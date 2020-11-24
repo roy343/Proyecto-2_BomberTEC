4be83573a363eb827dfad940f2fca7d7480f94ef
Roy Chavarría <roychav99@gmail.com>
2020-11-24 17:46:38 -0600

    Base PowerUp

---
+++ BomberTEC/Assets/PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float increase = 10f;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+            Destroy(gameObject);    
+    }
+
+
+}
