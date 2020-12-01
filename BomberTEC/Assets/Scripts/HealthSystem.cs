b18b973480466f5a930d696c05530114d6d087ba
Roy Chavarría <roychav99@gmail.com>
2020-12-01 16:48:27 -0600

    Sistema de vida básico

---
+++ BomberTEC/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public int life;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life < 1)
+        {
+            Destroy(hearts[0].gameObject);
+           
+        }
+        else if (life < 2)
+        {
+            Destroy(hearts[1].gameObject);
+        }
+        else if (life < 3)
+        {
+            Destroy(hearts[2].gameObject);
+        }
+    }
+
+    public void Damage(int d)
+    {
+        life -= d;
+    }
+}
