e810ba78d3265501c134d9287985ee8217151a14
Dennis <alejimenezc@hotmail.com>
2020-12-02 01:05:38 -0600

    Script para la generacion de niveles (no terminado)

---
+++ BomberTEC/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+
+public class LevelGeneration : MonoBehaviour
+{
+    [Range(0, 100)]
+    public int iniChance;
+
+    [Range(1, 8)]
+    public int birthLimit;
+
+    [Range(1, 8)]
+    public int deathLimit;
+
+    [Range(1, 10)]
+    public int numR;
+    private int count = 0;
+
+    private int[,] terrainMap;
+    public Vector3Int tmapSize;
+
+
+    public Tilemap topMap;
+    public Tilemap botMap;
+    public Tile topTile;
+    public Tile botTile;
+
+    int width;
+    int height;
+
+    public void doSim(int numR)
+    {
+        clearMap(false);
+        width = tmapSize.x;
+        height = tmapSize.y;
+        
+        if (terrainMap == null)
+        {
+            terrainMap = new int[width, height];
+            initPos();
+        }
+
+        for(int i=0; i<numR; i++)
+        {
+            terrainMap = genTilePos(terrainMap);
+        }
+    }
+
+    public int[,] genTilePos(int[,] oldMap)
+    {
+        int[,] newMap = new int[width, height];
+        int neighb;
+        BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
+
+        for(int x=0; x<width; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                neighb = 0;
+                foreach(var b in myB.allPositionsWithin)
+                {
+                    if (b.x == 0 && b.y == 0) continue;
+                    if (x+b.x >= 0 && x+b.x < width && y+b.y < height)
+                    {
+                        neighb += oldMap[x + b.x, y + b.y];
+                    }
+                    else
+                    {
+                        neighb++;
+                    }
+                }
+                if(oldMap[x,y] == 1)
+                {
+                    if (neighb < deathLimit) newMap[x, y] = 0;
+                    else
+                    {
+                        newMap[x, y] = 1;
+                    }
+                }
+                if (oldMap[x, y] == 0)
+                {
+                    if (neighb > deathLimit) newMap[x, y] = 1;
+                    else
+                    {
+                        newMap[x, y] = 0;
+                    }
+                }
+            }
+        }
+        return newMap;
+    }
+    public void initPos()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;
+            }
+        }
+    }
+    void Start()
+    {       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clearMap(bool complete)
+    {
+        topMap.ClearAllTiles();
+        botMap.ClearAllTiles();
+
+        if (complete)
+        {
+            terrainMap = null;
+        }
+    }
+}
