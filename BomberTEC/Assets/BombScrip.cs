06fb712238b13c0a8ec4f3be87d05408d32de86d
Roy Chavarría <roychav99@gmail.com>
2020-12-08 17:21:21 -0600

    Bomba explosion base

---
+++ BomberTEC/Assets/BombScrip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScrip : MonoBehaviour
+{
+
+    public float timer = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Debug.Log("BOOM!");
+            Destroy(gameObject);
+        }
+    }
+}
