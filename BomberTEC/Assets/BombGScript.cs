d1a00a3543df391376c4de99cad60c119eb0239e
Roy Chavarría <roychav99@gmail.com>
2020-12-09 16:52:45 -0600

    Cambio menor

--- BomberTEC/Assets/BombGScript.cs
+++ BomberTEC/Assets/BombGScript.cs
@@ -15,7 +15,7 @@ public class BombGScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("The bomb has beem planted");
+            Debug.Log("The bomb has been planted");
             Vector3 position = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
             Vector3Int cell = tilemap.WorldToCell(position);
             Vector3 center = tilemap.GetCellCenterWorld(cell);
