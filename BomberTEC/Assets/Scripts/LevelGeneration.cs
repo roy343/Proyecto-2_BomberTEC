e81c0fc3fce24489c137481831f6a3014681c8f1
Dennis <alejimenezc@hotmail.com>
2020-12-09 21:40:01 -0600

    Fix

--- BomberTEC/Assets/Scripts/LevelGeneration.cs
+++ BomberTEC/Assets/Scripts/LevelGeneration.cs
@@ -5,7 +5,7 @@ using UnityEngine.Tilemaps;
 using UnityEditor;
 
 
-public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bloques
+public class LevelGeneration : MonoBehaviour
 {
     [Range(0, 100)]
     public int iniChance;
@@ -19,55 +19,51 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
     [Range(1, 10)]
     public int numR;
 
-    private int[,] terrainMap;//Matriz que representa el mapa
+    private int count = 0;
 
-    public Vector3Int tmapSize;//Tamaño del mapa
+    private int[,] terrainMap;
+    public Vector3Int tmapSize;
 
-    //Vector3 PlayerPosition;
 
-    public Tilemap topMap;//El tilemap superior (donde estan los bloques)
-    public Tilemap botMap;//El tilemap inferior (el piso)
+    public Tilemap topMap;
+    public Tilemap botMap;
+    public Tile topTile;
+    public Tile botTile;
 
-    public Tile topTile;// Tiles superiores
-    public Tile botTile; // Tiles inferiores
-
-    int width; // Alto y ancho del mapa
+    int width;
     int height;
 
-    public void doSim(int numR)//Funcion para generar el mapa
+    public void doSim(int numR)
     {
         clearMap(false);
-
-        width = tmapSize.x;// Define el largo y el ancho
+        width = tmapSize.x;
         height = tmapSize.y;
         
-        if (terrainMap == null)// Si el mapa esta vacio crea uno nuevo
+        if (terrainMap == null)
         {
-            terrainMap = new int[width, height];// Crea el mapa encargado de
+            terrainMap = new int[width, height];
             initPos();
         }
 
-        for(int i = 0; i < numR; i++)// Aplica el algoritmo de generacion al mapa
+        for(int i = 0; i < numR; i++)
         {
             terrainMap = genTilePos(terrainMap);
-            Debug.Log(terrainMap[18, 1]);
         }
 
         for(int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                if(terrainMap[x,y] == 1)// Recorre el mapa posicion por posicion asignandole los tiles a cada una
+                if(terrainMap[x,y] == 1)
                 {
                     topMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), topTile);
                     botMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), botTile);
                 }
             }
         }
-        Debug.Log(terrainMap[18, 1]);
     }
 
-    public int[,] genTilePos(int[,] oldMap)// Algoritmo para la generacion organica de niveles
+    public int[,] genTilePos(int[,] oldMap)
     {
         int[,] newMap = new int[width, height];
         int neighb;
@@ -78,7 +74,7 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
             for (int y = 0; y < height; y++)
             {
                 neighb = 0;
-                foreach (var b in myB.allPositionsWithin)
+                foreach(var b in myB.allPositionsWithin)
                 {
                     if (b.x == 0 && b.y == 0) continue;
                     if (x + b.x >= 0 && x+b.x < width && y+b.y >= 0 && y+b.y < height)
@@ -92,7 +88,6 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
                 }
                 if(oldMap[x,y] == 1)
                 {
-                    if (x == 18 & y == 1) newMap[x, y] = 0;
                     if (neighb < deathLimit) newMap[x, y] = 0;
                     else
                     {
@@ -101,7 +96,7 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
                 }
                 if (oldMap[x, y] == 0)
                 {
-                    if (neighb > deathLimit & x != 18 & y != 1) newMap[x, y] = 1;
+                    if (neighb > deathLimit) newMap[x, y] = 1;
                     else
                     {
                         newMap[x, y] = 0;
@@ -109,19 +104,15 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
                 }
             }
         }
-        Debug.Log(newMap[18, 1]);
         return newMap;
     }
-    public void initPos()// Esto se encarga de darle a cada posicion en la matriz un 1 o un 0. Dependiendo de lo que tenga le asigna un tile o no
+    public void initPos()
     {
-        //PlayerPosition = new Vector3(3f, 5f, 0.0f);
-        //Vector3Int cell = botMap.WorldToCell(PlayerPosition);
-
-        for (int x = 0; x < width; x++)
+        for(int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;//Genera una posibilidad aleatoria de darle a la posicion en la matriz un 1 o un 0
+                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;
             }
         }
     }
@@ -129,18 +120,18 @@ public class LevelGeneration : MonoBehaviour //Probabilidades de aparicion de bl
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))// Genera el mapa con un click
+        if (Input.GetMouseButtonDown(0))
         {
             doSim(numR);
         }
 
-        if (Input.GetMouseButtonDown(1)) // Lo elimina con click derecho
+        if (Input.GetMouseButtonDown(1))
         {
             clearMap(true);
         }
     }
 
-    public void clearMap(bool complete) // Funcion para borrar el mapa
+    public void clearMap(bool complete)
     {
         topMap.ClearAllTiles();
 
